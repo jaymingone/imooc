646e1ceb26730f53cdea9a47775b964d911248e5
limingjie <393366088@qq.com>
2016-04-27 16:47:14 +0800

    123

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine','jade');/*设置默认的模板引擎*/
 app.use(express.static(path.join(__dirname,"public")));/*具体设置静态资源的路径位置*/
 // app.use(serveStatic('bower_components');
 app.locals.moment = require('moment');/*引入Moment模块格式化本地时间并赋值给app对象的本地对象调用*/
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.listen(port);/*监听指定的端口*/
 console.log('imooc started on port' + port);
