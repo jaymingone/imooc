10632df396ebfce09c314cf5fcbc4d5c2b45137b
limingjie <393366088@qq.com>
2016-05-17 22:22:42 +0800

    录入员分类问题

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -97,21 +97,37 @@
 				// summary:movieObj.summary,
 				// flash:movieObj.flash
 			);
-			var categoryId = _movie.category;
+			var categoryId = movieObj.category;
+			var categoryName = movieObj.categoryName;_
 			_movie.save(function(err,movie){
 				if(err){
 						console.log(err);
 				}
-				Category.findById(categoryId,function(err,category){
-					category.movies.push(movie._id);
-					category.save(function(err,category){
-						movie.category = category._id;
-						movie.save(function(err,movie){
-							res.redirect('/movie/'+movie._id);
+				if(categoryId){
+
+					Category.findById(categoryId,function(err,category){
+						category.movies.push(movie._id);
+						category.save(function(err,category){
+							movie.category = category._id;
+							movie.save(function(err,movie){
+								res.redirect('/movie/'+movie._id);
+							})
+								
 						})
-							
 					})
-				})
+				}else if(categoryName){
+					var category = new Category({
+						name:categoryName,
+						movies:[movie._id]
+					});
+					category.save(function(err,category){
+							movie.category = category._id;
+							movie.save(function(err,movie){
+								res.redirect('/movie/'+movie._id);
+							})
+								
+						});
+				}
 			});
 		}
 	};
